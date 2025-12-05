f4a1bd46c66ee1fcc9e9f9f932548ec6335fb0c1
ronniegikandi <kiiruronnie42@gmil.com>
2025-12-05 14:52:58 +0300

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,20 @@
 
 class Dog:
     # Class body goes here
+      def bark(self):
+        print("Woof!")  
+        
+      def sit(self):
+        print("The dog is sitting.")
+
 
     #Instance method definition
-    pass
+
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+snoopy.sit()
+pass
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,14 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
 
     #Instance method definition
-    pass
+Abby = Person()
+Abby.talk()
+Abby.walk()
+pass
